f362c0eb18d928a37aea5c5fa4133d29d4eb9f0b
irem <iremaydinn.7@gmail.com>
2022-11-09 23:14:06 +0300

    pinned

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Pin : MonoBehaviour
 {
-    public float speed = 20f;
+    private bool isPinned = false;
 
+    public float speed = 20f;
     public Rigidbody2D rb;
 
     void Update()
     {
+        //move if is not pinned 
         //move while checking physics
         //2dim vector 
-        rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        if (!isPinned)
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        
+
+    }
+
+    //hit the rotator and parent it
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Rotator")
+        {
+            //stop forward movement and stick
+            isPinned = true;
+        }
     }
 }
